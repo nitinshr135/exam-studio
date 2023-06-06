0690ceb781d3604fae1332fa4aa97c53f6b81ab0
Nitin Sharma <nitin@spheron.network>
2023-06-06 20:17:23 +0530

    Add og image, description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -24,29 +24,29 @@ export default function MyApp({ Component, pageProps }: any) {
           <meta name="theme-color" content="#3b82f6" />
           <meta
             name="description"
-            content="Generate memes using freeze frame images from James Q Quick's videos."
+            content="Next Gen online exam platform. Create and share your test with modern UI."
           />
           <link rel="canonical" href={process.env.NEXT_PUBLIC_APP_URL} />
           <meta name="twitter:card" content="photo" key="twitter-card" />
           <meta
             property="og:image"
-            content="https://res.cloudinary.com/jamesqquick/image/upload/c_fill,w_960,h_540,g_auto/l_text:Source%20Sans%20Pro_80_bold_stroke:What%20if%20I%20wrote,w_960,c_fit,co_white,bo_20px_solid_black/fl_layer_apply,fl_no_overflow,y_20,x_0,g_north/l_text:Source%20Sans%20Pro_80_bold_stroke:a%20Meme%20Generator%3F,w_960,c_fit,co_white,bo_20px_solid_black/fl_layer_apply,fl_no_overflow,y_20,x_0,g_south/f_auto/q_auto/v1/jqq-memes/50_nuzdle?_a=AVAAEDV0"
+            content="../../public/exam-studio-meta.jpg"
             key="og-image"
           />
           <meta
             property="twitter:image:alt"
-            content="Meme created with James Q Quick's face"
+            content="Next Gen online exam platform. Create and share your test with modern UI."
           />
           <meta
             property="twitter:image"
-            content="https://res.cloudinary.com/jamesqquick/image/upload/c_fill,w_960,h_540,g_auto/l_text:Source%20Sans%20Pro_80_bold_stroke:What%20if%20I%20wrote,w_960,c_fit,co_white,bo_20px_solid_black/fl_layer_apply,fl_no_overflow,y_20,x_0,g_north/l_text:Source%20Sans%20Pro_80_bold_stroke:a%20Meme%20Generator%3F,w_960,c_fit,co_white,bo_20px_solid_black/fl_layer_apply,fl_no_overflow,y_20,x_0,g_south/f_auto/q_auto/v1/jqq-memes/50_nuzdle?_a=AVAAEDV0"
+            content="../../public/exam-studio-meta.jpg"
             key="twitter-image"
           />
 
           <meta name="twitter:site" content={process.env.NEXT_PUBLIC_APP_URL} />
           <meta
             name="twitter:description"
-            content="Generate memes of James Q Quick"
+            content="Next Gen online exam platform. Create and share your test with modern UI."
           />
           <meta name="twitter:url" content={process.env.NEXT_PUBLIC_APP_URL} />
         </Head>
