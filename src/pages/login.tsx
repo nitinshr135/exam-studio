8fb80adc2d9a0465eff7bb582c373810731282fe
Nitin Sharma <nitin@spheron.network>
2023-06-09 03:32:14 +0530

    Remove unwanted code

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,12 +1,10 @@
 import LoginButton from "@/components/Button/login-button";
 import { useRouter } from "next/router";
 import { FormEvent, useEffect, useState } from "react";
-import { account } from "@/appwrite/appwriteConfig";
-import { v4 as uuidv4 } from "uuid";
 import Link from "next/link";
 import NavbarWithLogin from "@/components/Navbar/navbar-login";
-import InputText from "@/components/Input/input-text";
 import { UseUser } from "@/hooks/user-context";
+import InputText from "@/components/Input/input-text";
 
 export default function Login() {
   const { push } = useRouter();
